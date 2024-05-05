43a7696ac7f38c93129da6d8abf79cb5c50e621f
Tomi Amao <tomi@amao.io>
2024-05-05 01:14:03 +0100

    pong pong New Master

--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import "github.com/gin-gonic/gin"
 
 func main() {
+	port := os.Getenv("PORT")
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong pong pong ------ MASTER",
+			"message": "pong pong pong ------ NEW MASTER",
 		})
 	})
-	r.Run(":80")
+	r.Run(fmt.Sprintf(":%s", port))
 }
